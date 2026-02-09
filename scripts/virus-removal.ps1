265b113274a45fe858869f190107b5976e69095a
zbprog <zackberetskywatch@gmail.com>
2026-02-08 22:53:52 -0600

    Add initial virus removal PowerShell script

---
+++ scripts/virus-removal.ps1
@@ -0,0 +1,7 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function Invoke-VirusRemoval {
+    Write-Host 'Virus removal module loaded.'
+    Write-Host 'Add antivirus scan and cleanup steps here.'
+}
