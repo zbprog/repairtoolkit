59af92e6dade82b2642827e4f5226fff08bd4497
zbprog <zackberetskywatch@gmail.com>
2026-02-08 22:53:33 -0600

    Add system updates PowerShell script

---
+++ scripts/system-updates.ps1
@@ -0,0 +1,7 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function Invoke-SystemUpdates {
+    Write-Host 'System updates module loaded.'
+    Write-Host 'Add Windows update and driver update steps here.'
+}
